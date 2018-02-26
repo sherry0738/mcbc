3f9d0993ae1f96a849888a287d688c86d6e8970e
Sherry <lishuxian0738@gmail.com>
2018-02-26 17:03:21 +1100

    2nd try

--- app/components/App.js
+++ app/components/App.js
@@ -6,11 +6,28 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {info: 'getting from express...'}
   }
+
+
+componentDidMount() {
+  let localHost = 'http://localhost:3000/details'
+  let hostedDomain = 'https://rocky-thicket-78112.herokuapp.com/details'
+  fetch('https://rocky-thicket-78112.herokuapp.com/details').then(res => res.json()).then(res => this.setState({info: res.info}))
+}
+// fetch(hostedDomain, {
+//   body: JSON.stringify({info: 'from client'}),
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/json'
+//   }
+// })
+
 
   render() {
     return <div>
       <h1></h1>
+      <h2>{this.state.info}</h2>
       <h2>Mum coder support group</h2>
       <p>Not sure how to start or have a question? Just ask the question in our online support group, developers in our online community would love to help out and share their experience.</p>
       <p><a>Learn More</a></p>
@@ -24,7 +41,7 @@ export default class App extends React.Component {
       <h2>Resources</h2>
       <p>Giving back is always helpful. So join us to share you experience and help each other.</p>
       <p><a>Learn More</a></p>
-  </div>
+    </div>
   }
 
 }
