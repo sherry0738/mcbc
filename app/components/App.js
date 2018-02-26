ffbd6145c7535e6577ec2ef97a3c76e8a5921c68
Sherry <lishuxian0738@gmail.com>
2018-02-26 15:02:53 +1100

    1st try deploy frontend

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import PasswordInput from './PasswordInput'
+//import PasswordInput from './PasswordInput'
+import 
 
 export default class App extends React.Component {
 
@@ -9,10 +10,21 @@ export default class App extends React.Component {
 
   render() {
     return <div>
-      <h1>hello world</h1>
-      <label>example component</label>
-      <PasswordInput />
+      <h1></h1>
+      <h2>Mum coder support group</h2>
+      <p>Not sure how to start or have a question? Just ask the question in our online support group, developers in our online community would love to help out and share their experience.</p>
+      <p><a>Learn More</a></p>
     </div>
+    <div>
+      <h2>Resources</h2>
+      <p>go to find out what you .</p>
+      <p><a>Learn More</a></p>
+    </div>
+    <div>
+      <h2>Resources</h2>
+      <p>Giving back is always helpful. So join us to share you experience and help each other.</p>
+      <p><a>Learn More</a></p>
+  </div>
   }
 
 }
