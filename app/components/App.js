9936df815a7e5deda74649fe7c9348e649361737
sherry0738 <33485908+sherry0738@users.noreply.github.com>
2018-04-16 20:44:38 +1000

    Update App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,30 +1,20 @@
 import React from 'react'
 //import PasswordInput from './PasswordInput'
-import 
+import PasswordInput from './PasswordInput'
 
 export default class App extends React.Component {
-
-  constructor(props) {
-    super(props)
+  
+    constructor(props) {
+      super(props)
+    }
+  
+    render() {
+      return <div>
+        <h1>hello world</h1>
+        <label>example component</label>
+        <PasswordInput />
+      </div>
+    }
+  
   }
 
-  render() {
-    return <div>
-      <h1></h1>
-      <h2>Mum coder support group</h2>
-      <p>Not sure how to start or have a question? Just ask the question in our online support group, developers in our online community would love to help out and share their experience.</p>
-      <p><a>Learn More</a></p>
-    </div>
-    <div>
-      <h2>Resources</h2>
-      <p>go to find out what you .</p>
-      <p><a>Learn More</a></p>
-    </div>
-    <div>
-      <h2>Resources</h2>
-      <p>Giving back is always helpful. So join us to share you experience and help each other.</p>
-      <p><a>Learn More</a></p>
-  </div>
-  }
-
-}
